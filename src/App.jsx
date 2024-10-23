3e0793473cb53a880ae5ef752dd0d8bbd1e93092
Arian Immen <Arianimmen6@gmail.com>
2024-10-23 21:57:56 +0330

    adding the invoice overview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header";
+import InvoiceOverview from "./components/InvoiceOverview";
 import "./tailwind.css";
 
 function App() {
   return (
-    <div className="p-2 md:p-9">
+    <div className="p-2 md:p-9 container mx-auto max-w-screen-2xl w-full">
       <Header />
+      {/* main */}
+      <div className="relative flex flex-col items-center">
+        <InvoiceOverview />
 
-
+        {/* Main__Body */}
+        <div className="bg-slate-100 h-52 relative top-10 w-full  rounded-2xl"></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/InvoiceOverview.jsx
@@ -0,0 +1,68 @@
+function InvoiceOverview() {
+  return (
+    <div className="absolute top-0 z-10 w-full px-2 md:px-7">
+      <div
+        className="bg-slate-50 p-3 flex rounded-2xl gap-x-1 md:gap-x-4 shadow-[0_1px_4px_rgba(56,119,194,0.05),_0_-4px_20px_rgba(56,119,194,0.08)]z
+"
+      >
+        <InvoiceInfo />
+        <InvoiceAmount />
+        <InvoiceNumber />
+      </div>
+    </div>
+  );
+}
+
+export default InvoiceOverview;
+
+function InvoiceInfo() {
+  return (
+    <div className="w-1/2 flex flex-col gap-y-1 bg-slate-100 py-3 px-4 rounded-lg justify-center sm:w-1/3 lg:px-9">
+      <span className="text-xs font md:text-base">Invoice to:</span>
+
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="placeholder:text-sm md:placeholder:text-base text-sm md:text-base font-bold"
+        ></input>
+      </div>
+
+      <input
+        type="text"
+        className="text-xs md:text-base text-slate-500"
+        placeholder="Address"
+      ></input>
+    </div>
+  );
+}
+
+function InvoiceAmount() {
+  return (
+    <div className="hidden sm:flex flex-col items-end gap-y-2 bg-slate-50 border border-slate-200 py-4 px-4 rounded-lg justify-center sm:w-1/3 lg:px-9">
+      <span className="text-xs">Amount due:</span>
+      <div className="flex flex-col items-end">
+        <span className="text-xl font-bold md:text-2xl">$ 19,570.00</span>
+        <span className="font-bold text-slate-400 md:text-xl">USD</span>
+      </div>
+      <span className="font-bold text-slate-400 text-sm md:text-base">
+        July 26, 2024
+      </span>
+    </div>
+  );
+}
+
+function InvoiceNumber() {
+  return (
+    <div className="w-1/2 gap-y-2 flex flex-col sm:w-1/3">
+      <div className="flex flex-col text-xs py-3 px-4 rounded-lg bg-slate-100 md:text-base">
+        <span className="">Invoice number:</span>
+        <input type="text" className="font-bold" placeholder="Number"></input>
+      </div>
+      <div className="flex flex-col text-xs py-3 px-4 rounded-lg bg-slate-100 md:text-base">
+        <span>Issued:</span>
+        <span className="font-bold">June 26, 2024</span>
+      </div>
+    </div>
+  );
+}
