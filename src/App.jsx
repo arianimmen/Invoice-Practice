fc5195bb2b86cdb6da2d81a06b982baf39de2549
Arian Immen <Arianimmen6@gmail.com>
2024-10-23 20:37:09 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import "./tailwind.css";
+
+function App() {
+  return (
+    <div className="p-2 md:p-9">
+      <Header />
+
+
+    </div>
+  );
+}
+
+export default App;
