b0a4037ec8d55d9bba357cf822ee72778d4d5228
Arian Immen <Arianimmen6@gmail.com>
2024-10-25 20:16:45 +0330

    bug fixing

--- src/components/InvoiceMainData.jsx
+++ src/components/InvoiceMainData.jsx
@@ -144,7 +144,10 @@ function TableItem({ item, handleDelete }) {
       </td>
       <td className="text-center text-sm md:text-base min-w-20 p-3 whitespace-nowrap rounded-tr-lg rounded-br-lg flex items-center justify-center">
         <div className="mr-2"> $ {item.price * item.quantity}</div>
-        <TrashIcon onClick={() => handleDelete(item.id)} className="size-4 cursor-pointer" />
+        <TrashIcon
+          onClick={() => handleDelete(item.id)}
+          className="size-4 cursor-pointer text-red-500"
+        />
       </td>
     </tr>
   );
